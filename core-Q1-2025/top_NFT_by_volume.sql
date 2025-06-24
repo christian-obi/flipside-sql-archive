518adf076d8fa37da47c701924c96eb0185c5e34
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-24 22:35:53 +0100

    Create top_NFT_by_volume.sql

---
+++ core-Q1-2025/top_NFT_by_volume.sql
@@ -0,0 +1,28 @@
+with nft_table as(
+SELECT
+nft.block_timestamp::date as tx_date,
+nft.tx_hash as transaction,
+nft.from_address as seller,
+nft.to_address as buyers,
+nft.contract_address as nft_contract,
+nft.token_id as token_id,
+nft.name as nft_name,
+nft.origin_function_signature as nft_signature,
+tr.value_precise as nft_price,
+tr.tx_fee_precise as tx_fee
+FROM core.nft.ez_nft_transfers nft
+JOIN core.core.fact_transactions tr 
+on transaction = tr.tx_hash
+and nft_signature = tr.origin_function_signature
+ORDER by 1 DESC
+
+)
+SELECT
+DISTINCT nft_contract as n_nft_contracts,
+nft_name,
+round(sum(nft_price),1) as "NFT_VOLUME🟠",
+FROM nft_table
+where tx_date >= '2025-01-01'
+group by 1,2
+order by 3 DESC
+limit 15
