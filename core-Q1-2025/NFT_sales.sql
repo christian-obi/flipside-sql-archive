ba7835d858dc6afef758e3054897068ec411ad8b
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-24 22:47:14 +0100

    Update NFT_sales.sql

--- core-Q1-2025/NFT_sales.sql
+++ core-Q1-2025/NFT_sales.sql
@@ -1,23 +1,51 @@
-with bridging_out as(
-SELECT
-day,
-case when tx_count = tx_count then -tx_count end as bridge_out,
-FROM $query('8dcce56d-6022-4be5-aa5b-437d176304e8')
-order by day
-),
-bridging_in as(
-select
-day,
-tx_count as bridge_in
-from $query('04848f75-2b41-4aa2-9b16-5c51267e4b21')
-order by day
+with nft_table as(
+  SELECT
+    nft.block_timestamp :: date as tx_date,
+    nft.tx_hash as transaction,
+    nft.from_address as seller,
+    nft.to_address as buyers,
+    nft.contract_address as nft_contract,
+    nft.token_id as token_id,
+    nft.name as nft_name,
+    nft.origin_function_signature as nft_signature,
+    tr.value_precise as nft_price,
+    tr.tx_fee_precise as tx_fee
+  FROM
+    core.nft.ez_nft_transfers nft
+    JOIN core.core.fact_transactions tr on transaction = tr.tx_hash
+    and nft_signature = tr.origin_function_signature
+  ORDER by
+    1 DESC
 )
 SELECT
-pt.day as date,
-pt.bridge_in as bridge_in,
-bo.bridge_out as bridge_out
-FROM  bridging_in pt
-JOIN  bridging_out bo
-on pt.day= bo.day
-WHERE date >= '2025-01-01'
-ORDER by date DESC
+  date_trunc('day', tx_date) as tx_date,
+  count(DISTINCT transaction) as nft_transactions,
+  count(DISTINCT seller) as sellers,
+  count(DISTINCT buyers) as buyer,
+  count(DISTINCT nft_contract) as n_nft_contracts,
+  avg(nft_price) as avg_nft_price,
+  sum(nft_price) as nft_volume,
+  sum(count(DISTINCT transaction)) over (
+    order by
+      date_trunc('day', tx_date)
+  ) as cum_nft_transaction,
+  sum(count(DISTINCT seller)) over (
+    order by
+      date_trunc('day', tx_date)
+  ) as cum_sellers,
+  sum(count(DISTINCT buyers)) over (
+    order by
+      date_trunc('day', tx_date)
+  ) as cum_buyers,
+  sum(sum(nft_price)) over (
+    ORDER BY
+      date_trunc('day', tx_date)
+  ) as cum_nft_volume
+FROM
+  nft_table
+where
+  tx_date >= '2025-01-01'
+group by
+  1
+order by
+  1 DESC
