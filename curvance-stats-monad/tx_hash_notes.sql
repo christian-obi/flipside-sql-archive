44b9f5d714dc5c2acccc964bffbc68aadf57cca8
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-25 23:34:26 +0100

    Create tx_hash_notes.sql

---
+++ curvance-stats-monad/tx_hash_notes.sql
@@ -0,0 +1,21 @@
+--repay of 50usdc
+--0xcd950b94a60fb3503e7436c96dc0ded6832aef40ddc7dfdfa33fe6578bfdf992 --tx hash of repayment
+
+--longBTC withdraw wbtc collateralized --0x01ec56fd5848127332e71578688c9153d9bceab6abcac211368f8671630c0ecd collateralized
+--longBTC deposit wbtc uncollateralized --0x23a76c45f340c9a6bfece6ada24ce4b1da98e45b0b79a0e310ec3d77368653e6
+
+--shortBTC deposit USDC collateralized 0xf7416d2da78b1109815c7f6f1dea77d0d026e134279e57bb6b110f7f3628e757
+
+
+--stakedETH deposit LUSD collateralized 0x03b923bd7dde83247e27081ce0747b81f839a7433e646b9e6be9f39a39fdc846
+
+--locked CVE to get veCVE 0x540573f0bfa4254e5bd9b8d6480b69f199e10cfcdb795e4e4d54b0b8a4dcbde0
+--combining locks (continous & 12months)(not compounding) 0x2a1af3a04c717ac19b42156db1cac204788abbb0c8f50f435dbf152906f26ec7
+--NB when i combined locks it aut went to 12months lock
+-- switching combin lock from 12months to continous 0xc1cf443e5b4526cf6e7ea2dfc01343ec444c3653e4bea638553b1519c7110275
+
+--getting all faucet 0x486f3a7fb2df7a9d3b8ceaf23c9b852a0b3a25cc36d2ce50f23635a4425df769
+--referral link https://monad.curvance.com/leaderboard/referrals?referral=realist
+
+--pump for gain address 0x12f4de1f9ff82407b0B98eAbee8EeE43241f80D1
+--depositing MON into pump for gain address 0x22b9640cfd92b37956662cbfe9c8badba3a7d52b098a31e377932fc5377d1d37
