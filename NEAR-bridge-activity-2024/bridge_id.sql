a21053a25222c4001ab391b6ac4bcb4dd3d03244
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-26 00:03:09 +0100

    Create bridge_id.sql

---
+++ NEAR-bridge-activity-2024/bridge_id.sql
@@ -0,0 +1,9 @@
+SELECT
+  DISTINCT bridge_address
+FROM
+  near.defi.ez_bridge_activity 
+
+--allbridge(bridge.a11bd.near)
+--rainbow(factory.bridge.near, prover.bridge.near, aurora)
+--multichain(mpc-multichain.near)
+--wormhole(portalbridge.near)
