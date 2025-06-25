b30e9ec1fd0e714a612e07de51a94c5c7c2aae2c
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-25 23:47:51 +0100

    Create base_bot_bridgers.sql

---
+++ NEAR-bridge-activity-2024/base_bot_bridgers.sql
@@ -0,0 +1,38 @@
+with base_daily_activity AS (
+  SELECT
+    DATE(block_timestamp) AS activity_date,
+    origin_from_address,
+    COUNT(tx_hash) AS daily_transactions
+  FROM
+    base.defi.ez_bridge_activity
+  WHERE
+    activity_date >= current_date -60
+  GROUP BY
+    activity_date,
+    origin_from_address
+),
+base_filtered_activity AS (
+  SELECT
+    origin_from_address
+  FROM
+    base_daily_activity
+  WHERE
+    daily_transactions >= 50 --  addresses with more than 50 transactions per day as bots
+),
+base_active_bridgers AS (
+  SELECT
+    DISTINCT origin_from_address
+  FROM
+    base.defi.ez_bridge_activity
+  WHERE
+    origin_from_address IN (
+      SELECT
+        origin_from_address
+      FROM
+        base_filtered_activity
+    )
+)
+SELECT
+  COUNT(DISTINCT b.origin_from_address) AS base_active_bridgers_count
+FROM
+  base_active_bridgers b
