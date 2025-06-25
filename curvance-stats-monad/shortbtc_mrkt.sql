35a6efa3a5529c787e63d1dfb8355c0facb7b26f
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-25 23:30:19 +0100

    Create shortbtc_mrkt.sql

---
+++ curvance-stats-monad/shortbtc_mrkt.sql
@@ -0,0 +1,33 @@
+with base as (
+  select
+    block_timestamp as tx_date,
+    tx_hash,
+    from_address as user,
+    value_precise,
+    tx_fee_precise as tx_fee_mon,
+    cumulative_gas_used,
+  from
+    monad.testnet.fact_transactions
+  where
+    0 = 0
+    and to_address = '0x9e7ebd0f8255f3a910bc77fd006a410e9d54ee36'
+    and origin_function_signature = '0xe8bbf5d7'
+    and tx_succeeded = 'TRUE'
+    and block_timestamp >= '2025-02-18'
+)
+select
+  date_trunc('day', tx_date) as tx_date,
+  count(distinct tx_hash) as total_transactions,
+  count(distinct user) as total_user,
+  sum(tx_fee_mon) total_fee_generated,
+  sum(cumulative_gas_used) as total_gas_used,
+  sum(total_transactions) over () as cumulative_tx,
+  --count(distinct user) over () as total_wallets,
+  sum(total_fee_generated) over () as cumulative_fee,
+  sum(total_gas_used) over () as cumulative_gas
+from
+  base
+group by
+  1
+order by
+  1 desc
