2a6922b0f99f33c4adf18114f775d8bcdfa2362e
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-26 15:24:57 +0100

    Create price.sql

---
+++ metaplex/price.sql
@@ -0,0 +1,19 @@
+WITH filtered_prices AS (
+  SELECT
+    HOUR,
+    PRICE
+  FROM
+    solana.price.ez_prices_hourly
+  WHERE
+    TOKEN_ADDRESS = 'METAewgxyPbgwsseH8T16a39CQ5VyVxZi9zXiDPY18m'
+)
+SELECT
+  DATE_TRUNC('day', HOUR) AS "Day",
+  AVG(PRICE) AS "Average ($) Price",
+  --MAX(PRICE) AS "Maximum($) Price"
+FROM
+  filtered_prices
+GROUP BY
+  1
+ORDER BY
+  1
