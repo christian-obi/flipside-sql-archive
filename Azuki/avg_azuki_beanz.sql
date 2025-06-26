8cb4fd88d2654487f3ac9099adc15b04437123aa
realist <127094247+christian-obi@users.noreply.github.com>
2025-06-26 16:10:27 +0100

    Create avg_azuki_beanz.sql

---
+++ Azuki/avg_azuki_beanz.sql
@@ -0,0 +1,47 @@
+-- avg sale azuki beanz
+
+--azuki => '0xed5af388653567af2f388e6224dc7c4b3241c544'  
+--azuki elementals => '0xb6a37b5d14d502c3ab0ae6f3a0e058bc9517786e'  
+--azuki red beans => '0x306b1ea3ecdf94ab739f1910bbda052ed4a9f949'  
+--azuki elemental beanz => '0x3af2a97414d1101e2107a70e7f33955da1346305'
+with price_stats as (
+  SELECT
+    min(price_usd) as min_price,
+    percentile_cont(0.5) within GROUP (
+      ORDER by
+        price_usd
+    ) as mid_price,
+    max(price_usd) as max_price
+  FROM
+    ethereum.nft.ez_nft_sales
+  where
+    nft_address = '0x306b1ea3ecdf94ab739f1910bbda052ed4a9f949'
+    AND block_timestamp >= '2024-08-01'
+),
+binned_sales as (
+  SELECT
+    price_usd,
+    date_trunc('day', block_timestamp) as date
+  FROM
+    ethereum.nft.ez_nft_sales
+  where
+    nft_address = '0x306b1ea3ecdf94ab739f1910bbda052ed4a9f949'
+    and block_timestamp >= '2024-08-01'
+) --avg_price_per_day as (
+select
+  date,
+  avg(price_usd) as avg_sale_price,
+  CASE
+    when avg_sale_price <= 515 then 'low sale price range'
+    when avg_sale_price > 515
+    and avg_sale_price <= 576 then 'mid sale price range'
+    when avg_sale_price > 576
+    and avg_sale_price <= 123446 then 'high sale price range'
+    else 'above max price'
+  end as price_bin,
+from
+  binned_sales
+group by
+  1
+ORDER by
+  1
